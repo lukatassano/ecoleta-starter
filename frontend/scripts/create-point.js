deb1d68d4387853a53a098553a6ceae1743abb5c
Luka Tassano <lukatassano070@gmail.com>
2020-06-04 02:48:19 -0300

    page create point

--- frontend/scripts/create-point.js
+++ frontend/scripts/create-point.js
@@ -24,13 +24,17 @@ function getCities() {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+  citySelect.innerHTML = "";
+  citySelect.disabled = true;
+
   fetch(url)
     .then((res) => res.json())
     .then((cities) => {
+      citySelect.innerHTML = "";
       const showCities = cities.map((city) => {
         citySelect.innerHTML =
           citySelect.innerHTML +
-          `<option value="${city.id}">${city.nome}</option>`;
+          `<option value="${city.nome}">${city.nome}</option>`;
 
         citySelect.disabled = false;
       });
@@ -40,3 +44,35 @@ function getCities() {
 }
 
 document.querySelector("select[name=uf").addEventListener("change", getCities);
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
+
+const collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  itemLi.classList.toggle("selected");
+
+  const itemId = itemLi.dataset.id;
+
+  const alreadySelected = selectedItems.findIndex((item) => item == itemId);
+
+  if (alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter((item) => item !== itemId);
+
+    selectedItems = filteredItems;
+  } else {
+    selectedItems.push(itemId);
+  }
+
+  console.log(selectedItems);
+
+  collectedItems.value = selectedItems;
+}
